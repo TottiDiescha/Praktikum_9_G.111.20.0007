b72579ae4e02bbf1e8c295521ca81470c1f7d269
TottiDiescha <dieschaerr08@gmail.com>
2023-07-12 19:16:54 +0700

    UAS

--- Main.java
+++ Main.java
@@ -1,25 +1,70 @@
 public class Main {
- public static void main(String[] args) {
- Buku buku1 = new Buku("B001", "Pemrograman Java");
- Buku buku2 = new Buku("B002", "Pemrograman C++");
- BukuLuar buku3 = new BukuLuar("B003", "Harry Potter");
- Petugas petugas1 = new Petugas("Totti", "P001");
- Peminjam peminjam1 = new Mahasiswa();
- peminjam1.setNamaPeminjam("Diescha");
- peminjam1.setKodePeminjam("M001");
- peminjam1.doLogin();
- //peminjam1.doLogout();
- try{
- Peminjaman peminjaman1 = new Peminjaman(peminjam1, petugas1, buku1);
- System.out.println("Peminjaman: " + peminjaman1.getPeminjam().getNamaPeminjam());
- System.out.println("Petugas: " + peminjaman1.getPetugas().getNamaPetugas());
- System.out.println("Buku: " + peminjaman1.getBuku().getNamaBuku());
- System.out.println("Nama Buku: " + peminjaman1.getBuku().getNamaBuku());
- } catch (Exception e) {
- System.out.println("terjadi kesalahan: " + e.getMessage());
- } finally {
- System.out.println("Pemograman Selesai");
- }
+    public static void main(String[] args) {
+        Buku buku1 = new Buku("B001", "Pemrograman Java");
+        Buku buku2 = new Buku("B002", "Pemrograman C++");
+        BukuLuar buku3 = new BukuLuar("B003", "Majalah Bobo");
+        Petugas petugas1 = new Petugas("mahendra", "P001");
+        Peminjam peminjam1 = new Mahasiswa();
+        Peminjam peminjam2 = new Mahasiswa();
+        peminjam1.setNamaPeminjam("totti");
+        peminjam1.setKodePeminjam("M001");
+        peminjam2.setNamaPeminjam("diescha");
+        peminjam2.setKodePeminjam("M002");
+        peminjam1.doLogin();
+        peminjam2.doLogin();
+        // peminjam1.doLogout();
+        try {
 
- }
+            Peminjaman peminjaman1 = new Peminjaman(peminjam1, petugas1, buku1);
+
+            System.out.println("Nama Peminjaman: " + peminjaman1.getPeminjam().getNamaPeminjam());
+            System.out.println("Nama Petugas: " + peminjaman1.getPetugas().getNamaPetugas());
+            System.out.println("Kode Peminjam: " + peminjaman1.getPeminjam().getKodePeminjam());
+            System.out.println("Nama Buku: " + peminjaman1.getBuku().getNamaBuku());
+            Denda denda1 = new Denda(peminjaman1, 2);
+            System.out.println("Tagihan Denda " + denda1.getTagihan());
+
+            System.out.println("=== Denda ditambahkan ! ===");
+
+            peminjam2.addAntiDenda();
+
+            System.out.println("Tagihan denda: " + denda1.getTagihan());
+
+            System.out.println("=== Denda dihapuskan ! ===");
+
+            peminjam2.removeAntiDenda();
+
+            System.out.println("Tagihan denda: " + denda1.getHapus());
+
+            System.out.println("======================================================================");
+
+            Peminjaman peminjaman2 = new Peminjaman(peminjam2, petugas1, buku3);
+
+            System.out.println("Nama Peminjaman: " + peminjaman2.getPeminjam().getNamaPeminjam());
+            System.out.println("Nama Petugas: " + peminjaman2.getPetugas().getNamaPetugas());
+            System.out.println("Kode Peminjam: " + peminjaman2.getPeminjam().getKodePeminjam());
+            System.out.println("Nama Buku: " + peminjaman2.getBuku().getNamaBuku());
+            Denda denda2 = new Denda(peminjaman2, 7);
+            System.out.println("Tagihan denda: " + denda2.getTagihan());
+
+            System.out.println("=== Denda ditambahkan ! ===");
+
+            peminjam2.addAntiDenda();
+
+            System.out.println("Tagihan denda: " + denda2.getTagihan());
+
+            System.out.println("=== Denda dihapuskan ! ===");
+
+            peminjam2.removeAntiDenda();
+
+            System.out.println("Tagihan denda: " + denda2.getHapus());
+
+        } catch (Exception e) {
+            System.out.println("terjadi kesalahan: " + e.getMessage());
+        } finally {
+            System.out.println("Pemograman Selesai");
+        }
+
+
+    }
 }
--- Peminjam.java
+++ Peminjam.java
@@ -1,22 +1,39 @@
 public abstract class Peminjam implements UserLogin {
- private boolean isLogin;
- @Override
- public void doLogin() {
- System.out.println("Peminjam berhasil login");
- isLogin = true;
- }
- @Override
- public void doLogout() {
- System.out.println("Peminjam berhasil logout");
- isLogin = false;
- }
+    private boolean isLogin;
+    private boolean isAntiDenda;
 
- @Override
- public boolean isLogin() {
- return isLogin;
- }
- public abstract void setKodePeminjam(String kodePeminjam);
- public abstract String getKodePeminjam();
- public abstract void setNamaPeminjam(String namaPeminjam);
- public abstract String getNamaPeminjam();
+    @Override
+    public void doLogin() {
+        System.out.println("Peminjam berhasil login");
+        isLogin = true;
+    }
+
+    @Override
+    public void doLogout() {
+        System.out.println("Peminjam berhasil logout");
+        isLogin = false;
+    }
+
+    @Override
+    public boolean isLogin() {
+        return isLogin;
+    }
+
+    public abstract void setKodePeminjam(String kodePeminjam);
+
+    public abstract String getKodePeminjam();
+
+    public abstract void setNamaPeminjam(String namaPeminjam);
+
+    public abstract String getNamaPeminjam();
+    
+    public void addAntiDenda() {
+        System.out.println("Tambah Denda");
+        isAntiDenda = true;
+    }
+
+    public void removeAntiDenda() {
+        System.out.println("Hapus Denda");
+        isAntiDenda = true;
+}
 }
